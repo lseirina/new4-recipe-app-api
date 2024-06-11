9f0f89c91f1a58d02b5e7acbe19ff52560d8a070
root <lseirina87@gmail.com>
2024-06-11 20:51:39 +0300

    Added user model

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,16 @@
+"""
+Database models
+"""
 from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionMixin,
+)
 
-# Create your models here.
+
+class User(AbstractBaseUser, PermissionMixin):
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
