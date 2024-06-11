356516a39e71ff4814346b6aafaf2b72f7f8ec26
root <lseirina87@gmail.com>
2024-06-11 13:40:10 +0300

    Added test wait for db delay

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -28,3 +28,17 @@ class CommandTests(SimpleTestCase):
         """Мы проверяем, что метод check был вызван один раз и с аргументом
             databases=['default']"""
         patched_check.assert_called_once_with(databases=['default'])
+
+    def test_wait_for_db_delay(self, patched_check):
+        """Test wait_for_db command with getting OperationalError."""
+        """Здесь side_effect настроен как список значений, вызывает исключения
+            Это значит, что при первых двух вызовах имитация будет поднимать Psycopg2Error,
+            при следующих трех — OperationalError, а на шестой вызов вернет True."""
+        # то же самое что такой список [Psycopg2Error, Psycopg2Error, OperationalError, OperationalError, OperationalError, True]
+        patched_check.side_effect = [Psycopg2Error] * 2 + \
+            [OperationalError] * 3 + True
+
+        call_command('wait_for_db')
+
+        self.assertEqual(patched_check.call_count, 6)
+        patched_check.assert_called_with(databases=['default'])
