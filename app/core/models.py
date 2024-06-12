e8e25304d83c3dc008d16d08dffa8c713de135b0
root <lseirina87@gmail.com>
2024-06-12 20:34:25 +0300

    Added raising error

--- app/core/models.py
+++ app/core/models.py
@@ -14,6 +14,8 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """Create, save and return a new user."""
+        if not email:
+            raise ValueError('User must have email')
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
@@ -31,4 +33,3 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
-    
