c8fb67efab5d7225c83628fc9468d70a8b6bcf34
root <lseirina87@gmail.com>
2024-06-13 14:54:02 +0300

    Added useradmin edit page

--- app/core/admin.py
+++ app/core/admin.py
@@ -11,6 +11,16 @@ class UserAdmin(BaseUserAdmin):
     """define pages for user admin."""
     ordering = ['id']
     list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Permissions', {'fields': (
+            'is_active',
+            'is_staff',
+            'is_superuser',
+        )}),
+        ('Important dates', {'fields': ('last_login',)}),
+    )
+    readonly_fields = ['last_login']
 
 
 admin.site.register(models.User, UserAdmin)
