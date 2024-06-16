b43f41c31e92a2c9658113591fe0a6610a2cc6f5
root <lseirina87@gmail.com>
2024-06-16 09:50:13 +0300

    Added fixed user serializer

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -7,10 +7,11 @@ from rest_framework import serializers
 
 class UserSerializer(serializers.ModelSerializer):
     """Serializer for user object."""
+
     class Meta:
         model = get_user_model()
         fields = ['email', 'password', 'name']
-        exta_kwargs = {'password': {'write_only': True, 'min_length': 5}}
+        extra_kwargs = {'password': {'write_only': True, 'min_length': 5}}
 
     def create(self, validated_data):
         """Create and return user with encrypted password."""
--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -32,9 +32,9 @@ class PublicUserAPITests(TestCase):
 
         res = self.client.post(CREATE_USER_URL, payload)
 
-        self.assertEqual(res.status_code, status.HTTP_201_CREATE)
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
         user = get_user_model().objects.get(email=payload['email'])
-        self.assertEqual(user.check_password(payload['password']))
+        self.assertTrue(user.check_password(payload['password']))
         self.assertNotIn('password', res.data)
 
     def test_craete_user_with_existed_email(self):
