5fe3ce037a3010fd7a8d25562bd9042ae2ca9cd0
root <lseirina87@gmail.com>
2024-06-19 16:26:32 +0300

    Added urls for recipe

---
+++ app/recipe/urls.py
@@ -0,0 +1,20 @@
+"""
+Urls mapping for the redipe app.
+"""
+from django.urls import (
+    path,
+    include,
+)
+
+from rest_framework import DefaultRouter
+
+from recipe import views
+
+router = DefaultRouter()
+router.register('recipes', views.RecipeViewSet)
+
+app_name = 'recipe'
+
+url_patterns = [
+    path('', include(router.urls))
+]
