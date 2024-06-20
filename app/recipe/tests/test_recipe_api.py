0a3fbf69302e9c2a97265b4cb2cc8caa04369461
root <lseirina87@gmail.com>
2024-06-20 14:40:36 +0300

    Added test update user returns error

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -150,3 +150,18 @@ class PrivatRecipeAPITests(TestCase):
         for k, v in payload.items():
             self.assertEqual(getattr(recipe, k), v)
         self.assertEqual(recipe.user, self.user)
+
+    def test_update_user_returns_error(self):
+        """Test changing recipe user results in error."""
+        recipe = create_recipe(user=self.user)
+        new_user = create_user(
+            email='newuser@example.com',
+            password='test123',
+        )
+        payload = {'user': new_user.id}
+        url = detail_url(recipe.id)
+        self.client.patch(url, payload)
+
+        recipe.refresh_from_db()
+        self.assertEqual(recipe.user, self.user)
+
