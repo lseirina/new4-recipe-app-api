ce015dc5c4b0ad016d784cc6d7ca3cddf23d2b29
root <lseirina87@gmail.com>
2024-06-21 22:22:53 +0300

    Added test retrieve list of tags

--- app/recipe/tests/test_tag_api.py
+++ app/recipe/tests/test_tag_api.py
@@ -14,13 +14,13 @@ from recipe.serializers import TagSerializer
 TAGS_URL = reverse('recipe:tag-list')
 
 
-def creat_user(email='test@eaxmple.com', password='test123'):
+def create_user(email='test@eaxmple.com', password='test123'):
     """Create an dreturn a new user."""
     return get_user_model().objects.create_user(email=email, password=password)
 
 
-class PrivateTagAPITests(TestCase):
-    """Test unaithenticated API requests."""
+class PublicTagAPITests(TestCase):
+    """Test unauthenticated API requests."""
 
     def setUp(self):
         self.client = APIClient()
@@ -30,3 +30,23 @@ class PrivateTagAPITests(TestCase):
         res = self.client.get(TAGS_URL)
 
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivatTagAPITest(TestCase):
+    """Test authorized API request."""
+    def setUp(self):
+        self.user = create_user()
+        self.client = APIClient()
+        self.client.force_authenticat(user=self.user)
+
+    def test_retrieve_tags(self):
+        """Test retrieving a list of tags."""
+        Tag.objects.create(user=self.user, name='Tag1')
+        Tag.objects.create(user=self.user, name='Tag2')
+
+        res = self.client.get(TAGS_URL)
+        tags = Tag.objects.all().order_by('-name')
+        serializer = TagSerializer(tags, many=True)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
