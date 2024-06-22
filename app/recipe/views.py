776a2ccf434dbc8e7a883371d5b92288f6eaced1
root <lseirina87@gmail.com>
2024-06-22 21:00:56 +0300

    Added features for deeleting tag

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -38,7 +38,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
         serializer.save(user=self.request.user)
 
 
-class TagViewSet(
+class TagViewSet(mixins.DeleteModelMixin,
                  mixins.UpdateModelMixin,
                  mixins.ListModelMixin,
                  viewsets.GenericViewSet):
