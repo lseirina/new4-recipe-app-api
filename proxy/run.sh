68d7ab71d5ed53778cbed0ddea4bf251a10749b5
root <lseirina87@gmail.com>
2024-07-01 21:55:37 +0300

    Added run.sh for proxy

---
+++ proxy/run.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+set -e
+# this command takes a template file, replaces any environment variable placeholders
+# with their actual values, and writes he output to 2 file, file used by Nginx.
+envsubst < /etc/nginx/default.conf.tpl > /etc/nginx/conf.d/default.conf
+# starts the Nginx web server
+nginx -g 'daemon off;' # This option tells Nginx to run in the foreground instead of as a background daemon. For Docker it is necessary for the container to keep running
