4281aadf88b2e764cc10056ef9e405e3ff84d7b6
root <lseirina87@gmail.com>
2024-07-04 14:27:59 +0300

    Added core view

--- app/core/tests/test_health_check.py
+++ app/core/tests/test_health_check.py
@@ -3,7 +3,7 @@ from django.test import TestCase
 from django.urls import reverse
 
 from rest_framework import status
-from rest_framework import APIClient
+from rest_framework.test import APIClient
 
 
 class HealthCheckTests(TestCase):
