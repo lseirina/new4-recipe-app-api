06bf60dcfaf613c8ce6b3ca2c81130f67504d2da
root <lseirina87@gmail.com>
2024-07-07 14:28:42 +0300

    Added scripts/run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-set -e # if fails one - everything fail
+set -e # if ine command fails - all commands fail
 
 python manage.py wait_for_db
-python manage.py collectstatic --noinput
+python manage.py collectstatic --noinput # collect all static and media files from different apps to one file
 python manage.py migrate
 
+# it is a command to run uwsgi, workers handle request
 uwsgi --socket :9000 --workers 4 --master --enable-threads --module app.wsgi
